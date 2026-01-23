b389f5d6095af695dd76c889e65f27297e9ed8a4
Fredy Orellana <feorella@Fredys-MacBook-Air.local>
2026-01-23 12:08:33 -0600

    got already working att control problem

---
+++ src/integrator.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+import mrp_functions
+
+
+def integrator(func_x, x_0, t_0, t_f, dt=0.1, norm_value=False, is_mrp=False):
+
+    state_summary = []
+
+    x_n = x_0
+    state_summary.append(x_n)
+    t = t_0
+
+    while t < t_f:
+
+        f = func_x(x_n, t)
+
+        x_n = x_n + f * dt
+
+        if norm_value:
+            x_n = x_n / np.linalg.norm(x_n)
+
+        if is_mrp:
+
+            if mrp_functions.mrp_is_long(x_n):
+                print("mrp is long: %.4f" % (np.linalg.norm(x_n)))
+                x_n = mrp_functions.get_shadow_mrp(x_n)
+
+        state_summary.append(x_n)
+
+        t += dt
+
+    return state_summary
+
+def runge_kutta(func, x_0, t_0, t_f, dt=0.01, norm_value=False, is_mrp=False):
+
+    state_summary = []
+
+    x_n = x_0
+    state_summary.append(x_n)
+    t = t_0
+
+    while t < t_f:
+
+        k1 = dt * func(x_n, t)
+        
+        k2 = dt * func(x_n + 0.5 * k1, t + 0.5 * dt)
+        
+        k3 = dt * func(x_n + 0.5 * k2, t + 0.5 * dt)
+        
+        k4 = dt * func(x_n + k3, t + dt)
+
+        x_n = x_n + (k1 + 2*k2 + 2*k3 + k4) / 6
+
+        if norm_value:
+            x_n = x_n / np.linalg.norm(x_n)
+
+        if is_mrp:
+
+            if mrp_functions.mrp_is_long(x_n[:3]):
+                x_n[:3] = mrp_functions.get_shadow_mrp(x_n[:3])
+
+        state_summary.append(x_n)
+
+        t += dt
+
+    return state_summary
---
+++ src/mrp_functions.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import helper_functions
+
+
+def mrp_is_long(mrp):
+
+    if np.linalg.norm(mrp) > 1:
+        return True
+
+    else:
+        return False
+
+def get_shadow_mrp(mrp):
+
+    return ( -1 * mrp ) / np.linalg.norm(mrp)**2
+
+def invert_mrp(mrp):
+
+    return -1 * mrp
+
+def mrp_to_dcm(mrp):
+
+    mrp_norm = np.linalg.norm(mrp)
+    mrp_tilde = helper_functions.get_tilde_matrix(mrp)
+
+    return np.eye(3) + ( 8 * mrp_tilde @ mrp_tilde - 4 * ( 1 - mrp_norm**2 ) * mrp_tilde ) / ( 1 + mrp_norm**2 )**2
+
+def quaternions_to_mrp(beta):
+
+    mrp = np.zeros(shape=(3))
+
+    mrp[0] = beta[1] / ( 1 + beta[0] )
+    mrp[1] = beta[2] / ( 1 + beta[0] )
+    mrp[2] = beta[3] / ( 1 + beta[0] )
+
+    return mrp
+
+def mrp_addition(mrp_2, mrp_1):
+
+    # {mrp_FB} = {mrp_FN} * {mrp_BN}
+    mrp_1_abs_squared = np.dot(np.abs(mrp_1), np.abs(mrp_1))
+    mrp_2_abs_squared = np.dot(np.abs(mrp_2), np.abs(mrp_2))
+
+    mrp =  ( ( 1 - mrp_1_abs_squared) * mrp_2 + ( 1 - mrp_2_abs_squared ) * mrp_1 - 2 * np.cross(mrp_2, mrp_1) ) \
+           / ( 1 + mrp_1_abs_squared * mrp_2_abs_squared - 2 * np.dot(mrp_1, mrp_2) )
+
+    if mrp_is_long(mrp):
+        return get_shadow_mrp(mrp)
+
+    else:
+        return mrp
+
+def plot_mrp_norm_vs_time(mrp_summary, total_time):
+    """
+    Plots the norm of MRPs as a function of time.
+
+    Parameters:
+        mrp_summary (ndarray): Array of MRP and state history from the integrator, shape (N, 6),
+                               where the first three columns correspond to the MRPs.
+        total_time (float): Total simulation time in seconds.
+    """
+    # Generate the time array based on the number of simulation steps
+    mrp_summary = np.array(mrp_summary)
+    num_steps = mrp_summary.shape[0]
+    time_array = np.linspace(0, total_time, num_steps)
+
+    # Compute the norm of the MRPs at each time step
+    mrp_norms = np.linalg.norm(mrp_summary[:, :3], axis=1)
+    
+    # Plot the MRP norm over time
+    plt.figure(figsize=(10, 6))
+    plt.plot(time_array, mrp_norms, label="MRP Norm", linewidth=2)
+    plt.xlabel("Time (s)", fontsize=14)
+    plt.ylabel("MRP Norm", fontsize=14)
+    plt.title("MRP Norm as a Function of Time", fontsize=16)
+    plt.grid(True)
+    plt.legend(fontsize=12)
